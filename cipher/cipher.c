4e36369a68fbe3bf032938536e9149229ffc8416
kumail074 <kumail074@proton.me>
2024-10-25 02:26:45 +0500

    second project

--- cipher/cipher.c
+++ cipher/cipher.c
@@ -4,19 +4,28 @@
 
 // in this, the replacing value is incremented by 1 (by default) or some number user enters
 int main(){
-    int c,n;
-    printf("enter number: ");
-    scanf("%d\n", &n);
-    srand(rand());
+    int c,sh;
+    char a, b;
+    printf("enter characters: ");
+    scanf("%c\t%c\n", &a, &b);
+    sh = a - b;
     while((c = getchar()) != EOF)
     {
         if(isalpha(c)){
-            if (toupper(c) >= 'A' && toupper(c) <= 'M')
-                c += n;
-            else
-                c -= n;
-        }
-        putchar(c);
+            if(isupper(c)){
+                c+=sh;
+                if(c > 'Z') c-=26;
+                if(c < 'A') c+=26;
+                putchar(c);
+                    }
+            else if(islower(c)) {
+                c+=sh;
+                if(c > 'z') c-=26;
+                if(c < 'a') c+=26;
+                putchar(c);
+                }
+           else
+              putchar(c); }
     }
     return 0;
 }
