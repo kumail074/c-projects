d20f1555b8f8e8be9a85e29e2d547d10c4585d4a
Kumail Haider <kumail074@proton.me>
2025-02-20 00:52:46 +0500

    hashFunc

--- kernel/uthash.h
+++ kernel/uthash.h
@@ -102,24 +102,87 @@ do {
 #define HASH_INITIAL_NUM_BUCKETS_LOG2 5U
 #define HASH_BKT_CAPACITY_THRESH 10U
 
-/* calculate the element whose hash handle address is hhp */
+/*  calculate the element whose hash handle address is hhp
+    hhp is pointer to UT_hash_handle inside user-defined struct
+
+    note:
+        it reverses the process of extracting UT_hash_handle from a struct
+        
+        uthash often stores only the hh pointer but sometimes need to recover
+        the original structure
+        
+        this calculation retrieves the base address of the structure that 
+        contains hh
+
+*/
 #define ELMT_FROM_HH(tbl, hhp) ((void*)(((char*)(hhp)) - ((tbl)->hho)))
 /* 
-  ((void*)(((char*)(hhp)) - (tbl->hho))
-  {(void*)[(char*)(hhp) - (tbl->hho)]}
+
+    (char*)(hhp)
+        this allows byte-wise pointer arithmetic
+    
+    ((char*)(hhp)) - (tbl->hho)
+        tbl->hho is hash handle offset, which tells us how far hh is
+        from the start of the struct
+        subtracting hho moves the pointer back to the beginning of struct 
+        that contains hhTh
+
+    ((void*)(((char*)(hhp)) - (tbl->hho))
+        used to get a generic pointer to the struct containing hh
 */
 
-/* calculate the hash handle from element address elp */
+/*  calculate the hash handle from element address elp 
+    
+    elp is pointer to the element in hash table
+    
+    tbl->hho is hash handle offset which is the byte offset where the
+    UT_hash_handle is stored inside the structure
+
+*/
 #define HH_FROM_ELMT(tbl,elp) ((UT_hash_handle*)(void*)(((char*)(elp)) + ((tbl)->hho)))
 /*
-  ((UT_hash_handle*)(void*)(((char*)(elp)) + ((tbl)->hho)))
-  (char*)(elp) - (tbl->hho)
-  converts into void pointer
-  (void*)[(char*)(elp) - (tbl->hho)]
+    ((UT_hash_handle*)(void*)(((char*)(elp)) + ((tbl)->hho)))
+
+    by adding hho to elp, we move the pointer where UT_hash_handle is located.
+        (char*)(elp) + (tbl->hho)
+    
+    converts into void*, usually done for safety or to follow generic pointer conventions.
+        (void*)[(char*)(elp) - (tbl->hho)]
+    
+    finally, point is casted into a UT_hash_handle*, making it point to the UT_hash_handle
+    structure inside the elp.
+        ((UT_hash_handle*)(void*)(((char*)(elp)) + ((tbl)->hho)))
 */
 
 
-#define HASH_ROLLBACK_BKT(hh, head, itemptrhh)
+#define HASH_ROLLBACK_BKT(hh, head, itemptrhh) 
+/*  restores the bucket count and resets the UT_hash_handle pointers when an item is 
+    removed from the hash table.
+    it corrects the bucket count in case of a failed insertion or rollback operation.
+*/
 
+do {
+    struct UT_hash_handle* hd_hh_item = itemptrhh; //local pointer
+    unsigned _hd_bkt; //to store the computed bucket index
+    HASH_TO_BKT(_hd_hh_item->hashv, (head)->hh.tbl->num_buckets, _hd_bkt); //computes which bucket an item belongs to
+    (head)->hh.tbl->buckets[_hd_bkt].count++; //increments the count of items in the computed bucket
+    _hd_hh_item->hh_next = NULL; //removes link to next element
+    _hd_hh_item->hh_prev = NULL; //removes link to previous element
+} while (0)
+
+#define HASH_VALUE(keyptr, keylen, hashv)
+do {
+    HASH_FUNCTION(keyptr, keylen, hashv);
+} while (0)
+
+#define HASH_FIND_BYHASHVALUE(hh, head, keyptr, keylen, hashval, out)
+do {
+    out = NULL;
+    if (head) {
+        unsigned _hf_bkt;
+        HASH_TO_BKT(hashval, head->hh.tbl->num_buckets, _hf_bkt);
+        
+    }
+}
 
 #endif
